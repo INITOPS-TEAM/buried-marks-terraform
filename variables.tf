5e3e614dc75c672f060642167f9da5665aa65b4d
pmv-pmv <pisarenkomariia@gmail.com>
2026-03-01 23:12:43 +0200

    fix:INIT-108 apply linter recommendations

--- variables.tf
+++ variables.tf
@@ -1,10 +1,18 @@
 variable "aws_region" {
+  type = string
   default = "eu-north-1"
 }
 
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
+variable "aws_access_key" {
+  type = string
+  sensitive = true
+}
+variable "aws_secret_key" {
+  type = string
+  sensitive = true
+}
 
 variable "bucket_name" {
+  type = string
   default = "buried-marks-media"
 }
